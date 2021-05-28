5200fcbbb4277f24f16c3d0a21a64ec3f2b3f16e
irinafilipenko <73819257+irinafilipenko@users.noreply.github.com>
2021-05-28 11:39:19 +0300

    spin-stop

--- src/js/openModal.js
+++ src/js/openModal.js
@@ -1,30 +1,27 @@
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 import 'spin.js/spin.css';
-import { Spinner } from 'spin.js';
-import { opts } from './spinner';
+
+import { ligtboxSpinn } from './spinner';
 
 function onContainerClick(event) {
-  // event.preventDefault();
+  event.preventDefault();
 
   if (event.target.nodeName !== 'IMG') {
     return;
   }
 
-  const changeImg = `<img src=${event.target.dataset.source} alt="${event.target.alt}" />`;
+  const changeImg = `<img class = "basiclightbox-img" src=${event.target.dataset.source} alt="${event.target.alt}" />`;
   const instance = basicLightbox.create(changeImg);
-  console.log(instance.element());
+
   instance.show();
-  const lightboxEl = instance.element().firstElementChild;
-  const modalSpinner = new Spinner(opts).spin(lightboxEl);
-  console.dir(modalSpinner);
+  const lightboxEl = instance.element();
+  const modalSpinner = ligtboxSpinn.spin(lightboxEl);
+  const basiclightboxImg = document.querySelector('.basiclightbox-img');
 
-  console.dir(event.target.onload);
-  // if (event.target.onload) {
-  //   modalSpinner.spin();
-  // }
-
-  // event.target.onload = () => modalSpinner.stop();
+  basiclightboxImg.addEventListener('load', event => {
+    modalSpinner.stop();
+  });
 }
 
 export { onContainerClick };
--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,21 +1,18 @@
 import 'spin.js/spin.css';
 import { Spinner } from 'spin.js';
-import * as basicLightbox from 'basiclightbox';
-import 'basiclightbox/dist/basicLightbox.min.css';
-console.log(basicLightbox);
 
 const opts = {
-  lines: 13, // The number of lines to draw
-  length: 38, // The length of each line
+  lines: 16, // The number of lines to draw
+  length: 50, // The length of each line
   width: 17, // The line thickness
-  radius: 45, // The radius of the inner circle
+  radius: 60, // The radius of the inner circle
   scale: 1, // Scales overall size of the spinner
   corners: 1, // Corner roundness (0..1)
   speed: 1, // Rounds per second
   rotate: 0, // The rotation offset
   animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
   direction: 1, // 1: clockwise, -1: counterclockwise
-  color: 'tomato', // CSS color or array of colors
+  color: '#eae6e4', // CSS color or array of colors
   fadeColor: 'transparent', // CSS color or array of colors
   top: '50%', // Top position relative to parent
   left: '50%', // Left position relative to parent
@@ -25,8 +22,4 @@ const opts = {
   position: 'absolute', // Element positioning
 };
 
-export { opts };
-
-// const target = document.querySelector('basicLightbox');
-// console.log(target);
-// const spinner = new Spinner(opts).spin(target);
+export const ligtboxSpinn = new Spinner(opts);
